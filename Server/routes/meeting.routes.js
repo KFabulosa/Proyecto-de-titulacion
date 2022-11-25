da36921ac7d851e44504b660b5e73b73eee539bd
unknown <julio.gonzalez7589@alumnos.udg.mx>
2022-11-24 21:37:47 -0600

    Implementar api de mercado pago

--- Server/routes/meeting.routes.js
+++ Server/routes/meeting.routes.js
@@ -2,9 +2,17 @@ const { Router } = require("express");
 const router = Router();
 
 const { createNewMeeting, getAllMeetings } = require("../controllers/Meeting.controller");
+const PaymentController = require("../controllers/Payment.controller");
+const PaymentService = require("../services/Payment.service");
+
+const PaymentInstance = new PaymentController(new PaymentService());
 
 // router.get("/meeting/add" );
 router.post("/meeting/add", createNewMeeting);
 router.get("/meeting", getAllMeetings);
+
+router.get("/meeting/payment", function (req, res, next) {
+  PaymentInstance.getPaymentLink(req, res);
+});
 
 module.exports = router;
