5b7525b9d71c8f5cb89808ce4fddd29a98f85a92
efrenruizrubio <efren282@outlook.es>
2023-01-21 13:31:32 -0600

    First commit on dev-efren

--- Server/index.js
+++ Server/index.js
@@ -3,6 +3,10 @@ require("dotenv").config();
 const app = require("./server");
 require("./database");
 
+app.get("/", (req, res) => {
+  res.send('<h1>My Node App</h1>');
+});
+
 app.listen(app.get("port"), () => {
   console.log("Server on port:", app.get("port"));
 });
